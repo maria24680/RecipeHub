10a65ebee443b94d4ddc61cf3385b5fc4f65a093
maria24680 <mariamila24680@gmail.com>
2026-06-29 15:38:25 +0600

    Fix auth client

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,12 @@
-import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
-})
+import { createAuthClient } from "better-auth/react";
 
-export const { signIn, signUp, useSession } = createAuthClient()
+export const authClient = createAuthClient({
+  baseURL: process.env.BETTER_AUTH_URL,
+ 
+  onError: (error) => {
+    console.error("Auth error:", error);
+  },
+});
+
+
+export const { signIn, signUp, useSession, signOut, getSession } = authClient;
